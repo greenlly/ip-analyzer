92001f72cb279ba8b2c0d75b06b9964dcb258edf
Sabzi Greenly <private@private>
2021-01-13 18:44:32 +0330

    Give address to save information

--- categorize_info.py
+++ categorize_info.py
@@ -14,10 +14,11 @@ DEFAULT_IPS_ADDRESS = "./banned-ips.list"
 DEFAULT_FIELDS = ["status", "message", "country", "countryCode", "region",
                 "regionName", "city", "isp", "org", "as", "query"]
 DEFAULT_SPECIAL_KEY = "country"
+DEFAULT_INFORMATION_SAVE_ADDRESS = "./information-ips.json"
 
 # Categorize a list of information of some IP
-def categorize_info(ips_addr: str=DEFAULT_IPS_ADDRESS,
-                    fields: list=DEFAULT_FIELDS, special_key: str=DEFAULT_SPECIAL_KEY):
+def categorize_info(ips_addr: str=DEFAULT_IPS_ADDRESS, fields: list=DEFAULT_FIELDS,
+        special_key: str=DEFAULT_SPECIAL_KEY, info_save_addr: str=DEFAULT_INFORMATION_SAVE_ADDRESS):
     try:
         ips_list = list()
         info_list = list()
@@ -32,9 +33,10 @@ def categorize_info(ips_addr: str=DEFAULT_IPS_ADDRESS,
                 info_list.append(info)
             else:
                 print(f"{fixed_ip} :: FAILED ! => {info['message']}")
-        with open(file="information-ips.json", mode="w") as f:
+        with open(file=info_save_addr, mode="w") as f:
             json.dump(fp=f, obj=info_list, indent=4)
             f.close()
+            print(f"** Information saved in: {info_save_addr} **")
         result = categorize(info_list=info_list, special_key=special_key)
         return(result)
 
@@ -46,8 +48,9 @@ def categorize_info(ips_addr: str=DEFAULT_IPS_ADDRESS,
 # Also can use this alone
 if __name__ == "__main__":
     ips_address = input("Enter address of IP list file (default: './banned-ips.list'): ")
-    fields_str = input("Enter fields. cut only with ','. Leave this empty if you want default:\n")
+    fields_str = input("Enter fields. Cut them only with ','. Leave this empty if you want default:\n")
     special_key = input("Enter what key of this list you want categorize (default: 'country'): ")
+    info_save_addr = input("Where you want to save this IPs information (default: './information-ips.json'): ")
     fields = list()
     if not ips_address:
         ips_address = DEFAULT_IPS_ADDRESS
@@ -57,8 +60,11 @@ if __name__ == "__main__":
         fields = fields_str.split(",")
     if not special_key:
         special_key = DEFAULT_SPECIAL_KEY
+    if not info_save_addr:
+        info_save_addr = DEFAULT_INFORMATION_SAVE_ADDRESS
     try:
-        result = categorize_info(ips_addr=ips_address, fields=fields, special_key=special_key)
+        result = categorize_info(ips_addr=ips_address, fields=fields,
+                special_key=special_key, info_save_addr=info_save_addr)
         print(f"{result=}")
     except Exception as error:
         result = {"error": error}
